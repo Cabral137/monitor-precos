6bd787a9fde117b1bf66b1c298f5b788715161cf
Victor <cabralvictor137@gmail.com>
2025-10-13 14:55:56 -0300

    Adição do seletor de titulo

--- src/tracker.py
+++ src/tracker.py
@@ -17,15 +17,15 @@ STORE_CONFIG = {
 
     "www.kabum.com.br": {
         "nome_loja": "Kabum",
-        "seletor_titulo": "",
+        "seletor_titulo": "h1.sc-a1f7a75-1",
         "seletor_preco": "json_ld",
-        "render_js": True
+        "render_js": False
     },
 
     "www.amazon.com.br": {
         "nome_loja": "Amazon",
-        "seletor_titulo": "",
-        "seletor_preco": "",
+        "seletor_titulo": "span#productTitle",
+        "seletor_preco": "span.a-price-whole",
         "render_js": True
     }
 
